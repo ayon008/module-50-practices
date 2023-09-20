509d8cfa8d536a4e5f948a1697d1e576c67bb0dc
ayon008 <shariar.ayon128@gmail.com>
2023-09-20 18:51:13 +0600

    finished-Without-local-Storage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Cart.css';
 const Cart = props => {
-    console.log(props.cart);
+    const shoppingCarts = props.cart;
+
+    let total = 0;
+    let shippingTotal = 0;
+    for (const shoppingCart of shoppingCarts) {
+        const price = shoppingCart.price;
+        const shippingCharge = shoppingCart.shipping;
+        total = total + price;
+        shippingTotal = shippingTotal + shippingCharge;
+    }
+    const tax = parseFloat((total * 0.07).toFixed(2));
+    const grandTotal = total + shippingTotal + tax;
+
     return (
         <div className='cart'>
             <h3 className='cart-title'>Order Summary</h3>
-            <p>Selected Itmes: {props.cart.length}</p>
-            <p>Total Price</p>
-            <p>Total Shipping Charge</p>
-            <p>Tax</p>
-            <h3>Grand Total</h3>
+            <p>Selected Items: {shoppingCarts.length}</p>
+            <p>Total Price: {total}</p>
+            <p>Total Shipping Charge: {shippingTotal}</p>
+            <p>Tax: {tax}</p>
+            <h4>Grand Total: {grandTotal}</h4>
         </div>
     );
 };
